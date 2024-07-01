68fda53c6bbe1f4ce1fa78a2dbd370ac2495543e
yasminksd02 <yasmindelmondes02@gmail.com>
2024-07-01 18:54:31 -0300

    Aula com SE simples 1.0.0
    Aprendemos como comentar em linha simples, em bloco, o que é variável, como ela atribui dados um print que exibe no formato string ou texto

---
+++ python_if.py
@@ -0,0 +1,23 @@
+"""
+este programa é uma estrutura  IF simples
+"""
+valor = 10 # a variável valor recebeu o número 10
+
+"""
+valor é a variável
+= atribui um dado à variável valor
+10 é um dado numérico
+"""
+if valor > 5:
+    
+    """
+    if é o mesmo que SE CONDIÇÃO
+    valor é a variável que recebeu um dado númerico
+    >5 se o dado for maiar que
+    """
+    print('O valor é maior que 5.')
+    
+    """
+    a infornação exibida no print pode ser entre aspas ou apósrofos
+    porque é um dado de tipo um texto, ou string. 
+    """
