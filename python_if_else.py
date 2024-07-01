3f9ba43278becefca7d01441b27b8ab75f1396c6
yasminksd02 <yasmindelmondes02@gmail.com>
2024-07-01 19:05:12 -0300

    condicional IF e ELSE 1.0.0
    trabalhando com IF e ELSE

---
+++ python_if_else.py
@@ -0,0 +1,9 @@
+"""
+com IF e ELSE podemos exibir duas respostas 
+a ESPERADA e a NÃO ESPERADA
+"""
+valor = 10
+if valor > 5: # resposta esperada - SE
+    print('O valor é maior que 5.')
+else: # resposta inesperada - CASO CONTRARIO
+    print('O valor não é maior que 5.')
